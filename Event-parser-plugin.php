c12c5c47c7047cdcfea268d68dc2bd78eafb78f8
Krzysztof Wędrowicz <krzysztof.wedrowicz@wp.pl>
2016-07-29 12:06:46 +0200

    added templates

--- Event-parser-plugin.php
+++ Event-parser-plugin.php
@@ -19,7 +19,8 @@ function events_shortcode( $atts ) {
 		'city' => null,
 		'coach' => null,
 		'type' => null,
-		'group' => null
+		'group' => null,
+		'template' => 'default'
 	), $atts );
 
 
@@ -47,42 +48,8 @@ function events_shortcode( $atts ) {
 		$events = $parser->getEvents();
 	usort($events, "cmp");
 
-	$output .= "<h2><strong>Kalendarium szkoleń</strong></h2>";
-	$output .= "<form id='target' action=\"{$shost}/mycart/add\" enctype='text/plain'>";
-	$output .= "<table><tr><th>Nazwa</th><th>Miasto</th><th>Dostępne do</th><th>Zostało</th><th>Cena</th><th>Ilość</th><th></th></tr>";
-	foreach($events as $event)
-	{
-		/*$output .= "<div class='event'><input type=\"checkbox\" id=\"checkbox{$event['id']}\" name=\"id[]\" value=\"{$event['id']}\" class='event_checkbox'><strong>{$event['name']} (".$parser->getCity($event).",".$parser->getDate($event).")</strong> ";
-		$output .= "Ilość: <input type=\"number\" id=\"quantity{$event['id']}\" data-eventid='{$event['id']}'><br/>";
-		$output .= "Zostało: {$event['on_hand']} ";
-		$price = $event['price']/100;
-		$output .= "Cena: $price PLN</div>";*/
-		$output .= "<tr><td><input type=\"checkbox\" id=\"checkbox{$event['id']}\" name=\"id[]\" value=\"{$event['id']}\" class='event_checkbox'>{$event['name']}</strong></td>";
-		$output .= "<td>{$parser->getCity($event)}</td>";
-		$output .= "<td>{$parser->getDate($event)}</td>";
-		$output .= "<td>{$event['on_hand']}</td>";
-		$price = $event['price']/100;
-		$output .= "<td>$price PLN</td>";
-		$output .= "<td><input type=\"number\" id=\"quantity{$event['id']}\" data-eventid='{$event['id']}'></td></tr>";
-		$output .= "<td>";
-		if($event['url'])
-			$output .= "<a href=\"{$event['url']}\">Więcej</a>";
-		$output .= "</td>";
-	}
-	$output .= "</table>";
-	$output .= "<h2>Kupon</h2>";
-	$output .= "<input type=\"text\" id=\"coupon\"/><br/>";
-	$output .= "<p id='coupon_result'></p>";
-	$output .= "<h2><strong>Formularz rejestracyjny<strong></h2>";
-	$output .= "<div class='customer-data'><label for='customer_name'>Imię</label><input type='text' name='customer_name' required><br>";
-	$output .= "<label for='customer_surname'>Nazwisko</label><input type='text' name='customer_surname' required><br>";
-	$output .= "<label for='customer_phone'>Telefon</label><input type='text' name='customer_phone' required><br>";
-	$output .= "<label for='customer_email'>Email</label><input type='text' name='customer_email' required></div> ";
-	$output .= "<input type=\"checkbox\" required/>Akceptuję regulamin<br/>";
-	$output .= "<p style='font-size: 20px;'>Kwota na fakturze <span id=\"invoice_cost\">0</span> zł z VAT<br/>";
-	$output .= "<div class=\"errors\"></div><br/>";
-	$output .= "<input type=\"submit\" id=\"submit_button\" value=\"Zarejestruj się i zapłać\" class='et_pb_button  et_pb_button_0 et_pb_module et_pb_bg_layout_light'>";
-	$output .= "</form>";
+	include("templates/{$params['template']}.php");
+
 	$output .= "<script>window.backend_host =\"".$shost."\";</script>";
 	$output .= '<script src="'.plugin_dir_url( __FILE__ ).'js/js.cookie.js"></script>';
 	$output .= '<script src="'.plugin_dir_url( __FILE__ ).'js/invite_cookie.js"></script>';
@@ -91,6 +58,11 @@ function events_shortcode( $atts ) {
 	$output .= '<script src="'.plugin_dir_url( __FILE__ ).'js/calculate_cost.js"></script>';
 	$output .= '<script src="'.plugin_dir_url( __FILE__ ).'js/coupon_result.js"></script>';
 	return $output;
+
+}
+
+function template($name, $shost, $events)
+{
 
 }
 
